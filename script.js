bf095fe2ee7f7f1af5710209f98b98f9e1fc7310
g-battaglia <battaglia.giacomo@icloud.com>
2020-12-28 18:08:30 +0100

    END.

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger").addEventListener("click", () => {
+  document.querySelector(".main-menu").classList.toggle("open");
+  console.log(document.querySelector(".main-menu"));
+});
